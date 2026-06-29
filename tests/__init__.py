5a98f454d75996bb70a604ef7df5006773b57b33
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-06-28 23:55:02 -0300

    Sync generated workspace artifacts

--- tests/__init__.py
+++ tests/__init__.py
@@ -12,7 +12,7 @@ from flext_core.lazy import (
 )
 
 if _t.TYPE_CHECKING:
-    from flext_tests import td as td, tf as tf, tk as tk, tv as tv
+    from flext_tests import td as td, tf as tf, tk as tk, tm as tm, tv as tv
 
     from flext_quality import d as d, e as e, h as h, r as r, x as x
     from tests.base import (
@@ -99,6 +99,7 @@ _LAZY_IMPORTS = merge_lazy_imports(
                 "td",
                 "tf",
                 "tk",
+                "tm",
                 "tv",
             ),
         },
@@ -157,6 +158,7 @@ __all__: list[str] = [
     "td",
     "tf",
     "tk",
+    "tm",
     "tv",
     "u",
     "x",
